cfcb88e58266657ae116a8c0ea9cd6fcd7872ee6
ayemee <aimee.o.ardines@gmail.com>
2026-03-10 01:43:49 +0800

    initial commit: vite react starter

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(100)
+
+  return (
+    <div>
+      <h1>React Docs Breakdown Lab</h1>
+      <p>Starter app ready.</p>
+    </div>
+  )
+}
+
+export default App
