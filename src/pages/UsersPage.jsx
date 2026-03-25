2c813081e413c99e4196b39599b7408f9b5d1131
ayemee <aimee.o.ardines@gmail.com>
2026-03-25 21:30:59 +0800

    implement users API fetch with loading and error states

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,5 +1,56 @@
+import { useEffect, useState } from "react";
+
 function UsersPage() {
-    return <h2>Users Page</h2>
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  async function fetchUsers() {
+    try {
+      setLoading(true);
+      setError(null);
+      const res = await fetch("https://jsonplaceholder.typicode.com/users");
+
+      if (!res.ok) {
+        throw new Error("Failed to fetch users");
+      }
+
+      const data = await res.json();
+
+      setUsers(data);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      <h2>Users Lab</h2>
+      <button onClick={fetchUsers} style={{ marginBottom: "12px" }}>
+        {" "}
+        Refetch
+      </button>
+
+      {loading && <p>Loadinnngg....</p>}
+      {error && <p style={{ color: "red" }}></p>}
+
+      {!loading && !error && (
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>
+              {user.name} - {user.email}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default UsersPage;
